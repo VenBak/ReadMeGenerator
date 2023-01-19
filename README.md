8544fd3340b337d54188c40321ac683adb3677f7
VenBak <andre.brahin@gmail.com>
2023-01-19 14:40:18 -0500

    Created readme generator with prompts

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-#Title ${title}
+# readmegenerator
+
+## Description
+
+It is a command line interface that allows you t ocreate a quality readme file
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [License](#license)
+- [Contributions](#contributions)
+- [Usage](#usage)
+- [Test](#test)
+- [Contact](#contact)
+
+## Installation
+
+type node index.js
+
+## License
+
+MIT
+
+## Contributions
+
+please dont
+
+## Usage
+
+you have to install the  node  extenstin
+
+## Test
+
+npm test
+
+## Contact
+
+If you wish to perhaps get in touch with me you can contact me via my email address: andre.brahin@gmail.com .
+Or you can get in touch with me through my GitHub account: VenBak
--- index.js
+++ index.js
@@ -1,16 +1,87 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const generateReadMe = ({ title}) =>
-"#Title ${title}"
+const generateReadMe = ({title, description, installation, license, contribution, usage, test, username, email}) =>
+`# ${title}
+
+## Description
+
+${description}
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [License](#license)
+- [Contributions](#contributions)
+- [Usage](#usage)
+- [Test](#test)
+- [Contact](#contact)
+
+## Installation
+
+${installation}
+
+## License
+
+${license}
+
+## Contributions
+
+${contribution}
+
+## Usage
+
+${usage}
+
+## Test
+
+${test}
+
+## Contact
+
+If you wish to perhaps get in touch with me you can contact me via my email address: ${email} .
+Or you can get in touch with me through my GitHub account: ${username}
+`
 
 inquirer
   .prompt([{ 
     type: "input",
     name: "title",
-    message: "What is the title of your project",
+    message: "What is the title of your project ?",
+  },{ 
+    type: "input",
+    name: "description",
+    message: "How would you describe the project ?",
+  },{ 
+    type: "input",
+    name: "installation",
+    message: "What are the instructions to install the project ?",
+  },{ 
+    type: "input",
+    name: "license",
+    message: "Which license do you wish to use ?",
+  }, { 
+    type: "input",
+    name: "contribution",
+    message: "What guidelines do you wish to give to contributions ?",
+  }, { 
+    type: "input",
+    name: "usage",
+    message: "What information can you give about the usage of your project ?",
+  }, { 
+    type: "input",
+    name: "test",
+    message: "How would one run a test of a your project ?",
+  }, { 
+    type: "input",
+    name: "username",
+    message: "What is your GitHub username ?",
+  },  { 
+    type: "input",
+    name: "email",
+    message: "What is your email address ?",
   },
-
   ])
   .then((answers) => {
     const ReadMeContent = generateReadMe(answers)
